af393f1c48a2dbe3c741385763d6765e6d93953d
Alejandro Gomez <alejandrogomez88@gmail.com>
2020-03-15 18:39:27 -0400

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const { CheckerPlugin } = require('awesome-typescript-loader'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+
+module.exports = {
+  mode: "development",
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CheckerPlugin(),
+    new CleanWebpackPlugin(),
+  ]
+};
